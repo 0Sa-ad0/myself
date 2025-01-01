e034e347f67d6bd5b29fe264cc2d919695fd34c5
SAAD <96654865+0Sa-ad0@users.noreply.github.com>
2025-01-01 12:21:20 +0600

    OK

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/myself/app",
   assetPrefix: "/myself/app/",
-  // Other Next.js configurations
   output: "export",
 };
 
