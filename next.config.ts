60d93bef2c46fa87db3d2cf4c9f5bbfaec27df1e
SAAD <96654865+0Sa-ad0@users.noreply.github.com>
2025-01-16 18:22:47 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,4 +6,14 @@ const nextConfig: NextConfig = {
   output: "export",
 };
 
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+const basePath = isGithubPages ? '/myself' : '';
+const assetPrefix = isGithubPages ? '/myself/' : '';
+
+module.exports = {
+  basePath,
+  assetPrefix,
+};
+
 export default nextConfig;
